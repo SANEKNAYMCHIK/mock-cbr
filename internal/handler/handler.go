3ff287f1235ea639a8de5f23b8f9541aef96b30e
SANEKNAYMCHIK <sashanaumov053@yandex.ru>
2026-03-16 22:43:53 +0300

    Remove unneccessary comments

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -14,22 +14,14 @@ func GetRates(w http.ResponseWriter, r *http.Request) {
 	status := r.URL.Query().Get("status")
 	statusCode, err := strconv.Atoi(status)
 	if err != nil {
-		// log.Printf("Incorrect status in request")
 		http.Error(w, "Incorrect status in request", http.StatusInternalServerError)
 		return
 	}
 	if statusCode != http.StatusOK {
-		// log.Printf("unsuccessful data receiving")
 		http.Error(w, "Unsuccessful data receiving", http.StatusInternalServerError)
 		return
 	}
 	WriteResponse(w, services.GetRates(testId, statusCode))
-	// func(w http.ResponseWriter, r *http.Request) {
-
-	// 	w.Header().Set("Content-Type", "application/json")
-	// 	w.WriteHeader(http.StatusOK)
-	// 	w.Write([]byte(`{"USD": 1.0, "EUR": 0.85, "GBP": 0.75}`))
-	// }
 }
 
 func WriteResponse(w http.ResponseWriter, rate models.Rate) {
